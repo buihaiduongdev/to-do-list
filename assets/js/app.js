ee534926e099e54650ce8699dfa0f9d7ceb48056
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-07-31 21:23:42 +0700

    feat: add note creation and input validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,47 @@
-const API_BASE = 'http://localhost:3000/';
-const carsBlock = document.getElementById('cars-block');
 
-function create(resource, data) {
-    return fetch(`${API_BASE + resource}`, {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(data)
-    })
-        .then(res => res.json())
-        .catch(err => err);
+let index = 0;
+const noteBtn = document.getElementById('note-btn');
+const noteInput = document.getElementById('input-to-do');
+const takeNote = function () {
+    const noteText = noteInput.value;
+
+    if (!isInvalid()) return;
+    const toDoList = document.getElementById('to-do-list');
+    const liChildOf_toDoList = document.createElement('li');
+
+    const labelChildOf_li = document.createElement('label');
+    labelChildOf_li.setAttribute('for', `task-${index}`);
+    labelChildOf_li.innerText = noteText;
+
+    const inputChildOf_li = document.createElement('input');
+    inputChildOf_li.setAttribute('type', `checkbox`);
+    inputChildOf_li.setAttribute('id', `task-${index++}`);
+
+    liChildOf_toDoList.appendChild(labelChildOf_li);
+    liChildOf_toDoList.appendChild(inputChildOf_li);
+
+    liChildOf_toDoList.classList.add('to-do-item');
+    toDoList.appendChild(liChildOf_toDoList);
+    noteInput.value = '';
+    noteInput.focus();
 }
 
-function read(resource, id) {
-    return fetch(`${API_BASE + resource} / ${id}`)
-        .then(res => res.json())
-        .catch(err => err);
-}
+const isInvalid = function () {
+    const noteText = noteInput.value.trim();
+    if (!noteText) {
+        noteInput.classList.add('invalid');
+        noteInput.setAttribute('placeholder', 'Vui lòng nhập ghi chú');
+        return false;
+    }
+    noteInput.classList.remove('invalid');
+    noteInput.setAttribute('placeholder', '');
 
-function update(resource, id, newData) {
-    return fetch(`${API_BASE + resource}/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(newData)
-    })
-        .then(res => res.json())
-        .catch(err => err)
+    return true;
 }
+noteBtn.addEventListener('click', takeNote);
 
-function remove(resource, id) {
-    return fetch(`${API_BASE + resource}/${id}`, {
-        method: 'DELETE',
-    })
-        .then(res => res.json())
-        .catch(err => err)
-}
-// create('cars', {}).then(data => console.log(data))
-// read('cars').then(data => console.log(data));
-// update('cars', 'aaaa', { name: "1111" }).then(data => console.log(data));
-// remove('cars', 'c577').then(a => console.log(a));
+noteInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        takeNote();
+    }
+});
